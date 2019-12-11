2c6c812d21c3338ce3a1628cd45ca9c00931cb3b
Angela Dominguez <aodoming154@gmail.com>
2019-12-11 13:12:19 -0800

    Adding the app.py & scraping.py Files

---
+++ apps/app.py
@@ -0,0 +1,33 @@
+# Import Dependency Tools
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+# Set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Set Up Flask App Routes
+# (1.for the main HTML page everyone will visit   2.for scraping new data using the code we’ve written. )
+
+# Define the route for the HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# Set up our scraping route.
+#  It create's a "button" of the web app that will run the code, to scrape for updated data, when clicked
+@app.route("/scrape")                   # route flask will be using
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scrape_mars.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+# Tell flask to run
+if __name__ == "__main__":
+   app.run()
---
+++ apps/scraping.py
@@ -0,0 +1,80 @@
+
+#################################################### Article Scraping
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': 'chromedriver.exe'}
+browser = Browser('chrome', **executable_path)
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+# Optional delay for loading the page. Telling our browser to wait a second before searching for components:
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+# Set up the HTML parser
+html = browser.html
+news_soup = BeautifulSoup(html, 'html.parser')
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+# Scraping : assign the title and summary text to variables we’ll reference later.
+# In other words, identify the parent element and create a variable to hold it
+slide_elem.find("div", class_='content_title')
+
+# Search within the element for the title.And stripe the additional HTML attributes and tags with the use of .get_text().
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+# Next add the summary text. Use the unique class associated with the summary ie “article_teaser_body.”
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+################################################### Image Scraping
+# Set up the URL to visit the site
+url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(url)
+
+# Find and click the full image button, Use the id in our code
+full_image_elem = browser.find_by_id('full_image')      # the browser frinds an element with its id and hplds it in a var
+full_image_elem.click()                                 # splinter will "click" the image to view its full size
+
+
+# Use Splinter’s functionality ability to find elements using text.
+# Find the "more info" button using only text and "click" that
+browser.is_element_present_by_text('more info', wait_time=1)
+more_info_elem = browser.find_link_by_partial_text('more info')
+more_info_elem.click()
+
+# Scrape the full-size image URL. Parse the resulting html with soup
+html = browser.html
+img_soup = BeautifulSoup(html, 'html.parser')
+
+# Tags used to find the most recent image :  <figure /> and <a /> tags have the image link nested within them.
+# Use (<figure />, <a />, and <img />) to build the URL to the full-size image.  Find the relative image url
+img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+img_url_rel
+
+# Add the base URL to create an absolute URL
+img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+img_url
+
+
+# Scraping an entire table with Pandas’ .read_html() function.
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.columns=['description', 'value']
+df.set_index('description', inplace=True)
+df
+
+# Adding the DataFrame to a web application
+# Using Pandas convert DataFrame back into HTML-ready code using the .to_html()
+df.to_html()
+
+
+# Deactivating/Turning Off the automated browser session
+browser.quit()
