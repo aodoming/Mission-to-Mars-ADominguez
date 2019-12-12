fb4f2bddde4b9956a963669320bcdc89a7fc826e
Angela Dominguez <aodoming154@gmail.com>
2019-12-11 22:00:36 -0800

    Include app.py file

--- apps/app.py
+++ apps/app.py
@@ -24,7 +24,7 @@ def index():
 @app.route("/scrape")                   # route flask will be using
 def scrape():
    mars = mongo.db.mars
-   mars_data = scrape_mars.scrape_all()
+   mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
 
